cd5f2c861fca7bce2cc69010562922b0ff4a4847
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-04-30 10:28:21 +0530

    Create primary key.sql

---
+++ primary key.sql
@@ -0,0 +1,6 @@
+create database employee;
+show databases;
+create table details(name varchar(25),id varchar(8) not null primary key,salary int(8));
+insert into details values("brindha","15BEE077","25000");
+desc details;
+select * from details;
