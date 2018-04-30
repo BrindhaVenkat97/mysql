ef71c61e9306cc43bf57d021fe51d7417e99eef3
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-04-30 09:18:27 +0530

    Create insert into table and select two values from table.sql

---
+++ insert into table and select two values from table.sql
@@ -0,0 +1,5 @@
+create database brindha;
+show databases;
+create table details(name varchar(25),rollno varchar(8),college_name varchar(50),dept varchar(20));
+insert into details(name,rollno,college_name,dept) values("brindha","15BEE077","KCT","EEE");
+select name,dept from details;
