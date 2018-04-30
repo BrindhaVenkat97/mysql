ea1dfa2bc41537f67596f2167f4b216a0a0fda3a
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-04-30 09:17:32 +0530

    Create create database and create 10 fields.sql

---
+++ create database and create 10 fields.sql
@@ -0,0 +1,4 @@
+create database brindha;
+show databases;
+create table details(name varchar(25),rollno varchar(8) not null,age int(2),height float(5),weight float(3),college_name varchar(50),dept varchar(20),address varchar(50),mobile_num int(10),signature varchar(25));
+desc details;
