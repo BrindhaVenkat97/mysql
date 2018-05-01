fc529986c72148220415f8dac8e0de30216697ba
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-05-01 10:19:13 +0530

    Create create index.sql

---
+++ create index.sql
@@ -0,0 +1,9 @@
+create database guvi;
+show databases;
+create table details(id int not null primary key auto_increment,name varchar(25) not null,age int(5) null);
+insert into details(name,age) values("brindha","21");
+insert into details(name,age) values("riyan","3");
+insert into details(name,age) values("ananthi","27");
+insert into details(name,age) values("sudha","28");
+create index idx_name on details(name);
+desc details;
