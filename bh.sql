45f13ae4d611bf8afc8e6210d6f726be4aeaf39e
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-05-01 09:42:40 +0530

    Delete bh.sql

--- bh.sql
+++
@@ -1,7 +0,0 @@
-create database employee;
-show databases;
-create table details(id int(8) not null auto_increment,name varchar(25),age int(5));
-insert into details(name,age) values("brindha","21");
-insert into details(name,age) values("ananthi","27");
-insert into details(name,age) values("sudha","28");
-select * from details;
